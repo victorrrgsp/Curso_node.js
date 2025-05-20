4339d7ded5c1f9745c8ccdd5866b2da7731853b6
Victorgsp <victorgabrielpimenta52@gmail.com>
2025-05-20 01:36:16 -0300

    configurando a doocumentação da API com swagger e o typeorm

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -2,12 +2,28 @@ import  express  from "express"
 import cors from "cors"
 import {routes} from "./routes" 
 import { errorHandler } from "./middlewares/errorHandler"
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0'
+        },
+    },
+    apis: [],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
 
 // aqui é a instacia do express(mico fremeworlk)
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(routes)
 app.use(errorHandler)
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -6,5 +6,6 @@ import { app } from "./app";
 // quando executar a aplicação (npm run dev), onde está configurado no packege.json este arquivo (server), vai executar a aplicação que vai usar na port 3333 do servidor e irá mostar no console uma menssage
 app.listen(env.PORT, () => {
     console.log(`Server running on port ${env.PORT}`)
+    console.log('API docs avalable at GET /docs')
 })
 
